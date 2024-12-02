6a3b7e2193e208469e45dc9f7ceb9e97bbd021a2
Yaroslav-662 <paroslav662@gmail.com>
2024-12-03 01:44:10 +0200

    Add files via upload

---
+++ 13/13/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Lab13
+{
+    partial class fMain
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.tbx1min = new System.Windows.Forms.TextBox();
+            this.tbx1max = new System.Windows.Forms.TextBox();
+            this.tbdx1 = new System.Windows.Forms.TextBox();
+            this.tbx2min = new System.Windows.Forms.TextBox();
+            this.tbx2max = new System.Windows.Forms.TextBox();
+            this.tbdx2 = new System.Windows.Forms.TextBox();
+            this.gv = new System.Windows.Forms.DataGridView();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+            // gv
+            // 
+            this.gv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv.Location = new System.Drawing.Point(40, 120);
+            this.gv.Name = "gv";
+            this.gv.Size = new System.Drawing.Size(600, 250);
+            this.gv.TabIndex = 6;
+
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(40, 390);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 7;
+            this.btnCalc.Text = "Розрахувати";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(160, 390);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Очистити";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(280, 390);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Вийти";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "x1min (start)";
+            
+            // Additional Labels for UI...
+
+            this.Name = "fMain";
+            this.Text = "Lab13";
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox tbx1min;
+        private System.Windows.Forms.TextBox tbx1max;
+        private System.Windows.Forms.TextBox tbdx1;
+        private System.Windows.Forms.TextBox tbx2min;
+        private System.Windows.Forms.TextBox tbx2max;
+        private System.Windows.Forms.TextBox tbdx2;
+        private System.Windows.Forms.DataGridView gv;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ 13/13/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab13
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            // Parse the input values
+            double x1min = double.Parse(tbx1min.Text);
+            double x1max = double.Parse(tbx1max.Text);
+            double dx1 = double.Parse(tbdx1.Text);
+            double x2min = double.Parse(tbx2min.Text);
+            double x2max = double.Parse(tbx2max.Text);
+            double dx2 = double.Parse(tbdx2.Text);
+
+            // Calculate the number of rows and columns
+            int rows = (int)((x1max - x1min) / dx1) + 1;
+            int columns = (int)((x2max - x2min) / dx2) + 1;
+
+            // Initialize the DataGridView
+            gv.Rows.Clear();
+            gv.Columns.Clear();
+
+            // Add columns
+            for (int i = 0; i < columns; i++)
+            {
+                gv.Columns.Add("col" + i, "x2=" + (x2min + i * dx2).ToString());
+            }
+
+            // Add row for the data and calculate the values
+            for (int i = 0; i < rows; i++)
+            {
+                double x1 = x1min + i * dx1;
+                gv.Rows.Add();
+                gv.Rows[i].Cells[0].Value = "x1=" + x1.ToString();
+
+                double rowSum = 0;
+
+                for (int j = 0; j < columns; j++)
+                {
+                    double x2 = x2min + j * dx2;
+                    double value = x1 * x2; // Example calculation: x1 * x2
+                    gv.Rows[i].Cells[j].Value = value;
+                    rowSum += value;
+                }
+
+                // Add the row sum in the last column
+                gv.Rows[i].Cells[columns].Value = rowSum.ToString();
+            }
+
+            // Add the sum for each column in the last row
+            gv.Rows.Add();
+            gv.Rows[rows].Cells[0].Value = "Sum";
+
+            for (int j = 0; j < columns; j++)
+            {
+                double columnSum = 0;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    columnSum += Convert.ToDouble(gv.Rows[i].Cells[j].Value);
+                }
+
+                gv.Rows[rows].Cells[j].Value = columnSum.ToString();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear the grid and inputs
+            gv.Rows.Clear();
+            gv.Columns.Clear();
+            tbx1min.Clear();
+            tbx1max.Clear();
+            tbdx1.Clear();
+            tbx2min.Clear();
+            tbx2max.Clear();
+            tbdx2.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
